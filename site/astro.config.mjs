863694ff33f1757013d133ccdcb51a40d20c933c
Steve Freund <sdfreund10@gmail.com>
2026-09-17 07:51:07 -0700

    Add JS linting and github check

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import { siteUrl } from "./src/lib/site.ts";
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import { siteUrl } from './src/lib/site.ts'
 
 // https://astro.build/config
 export default defineConfig({
   site: siteUrl,
-  integrations: [sitemap()],
-});
+  integrations: [sitemap()]
+})
